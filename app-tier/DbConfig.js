c661244490ca83fa9943659d3085aec243c303f3
yash91989201 <yashraj6370@gmail.com>
2023-11-15 08:38:10 +0530

    update html file and add icon assets

--- app-tier/DbConfig.js
+++ app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
+  DB_HOST: "",
+  DB_USER: "master",
+  DB_PWD: "administrator",
+  DB_DATABASE: "administrator",
 });
